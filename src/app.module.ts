dbfd4f1b4db2c34598610d89dc2255885e5ac207
Ardiman Tando <tando.ardiman@gmail.com>
2024-05-28 22:38:55 +0800

    chore: change file name from app.module.ts to index.ts for better organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ApiModule } from './api/api.module';
+import { ApiModule } from './api';
 
 @Module({
   imports: [

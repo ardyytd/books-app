7e4d4f7d77a6039186abd5a4463a47ee19ba0c9c
Ardiman Tando <tando.ardiman@gmail.com>
2024-05-30 19:59:01 +0800

    feat: apply auth middleware

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 
 import { AuthModule } from './auth/auth.module';
 import { MemberModule } from './member/member.module';
 import { BookModule } from './book/book.module';
 import { BorrowModule } from './borrow/borrow.module';
+import { AuthenticationMiddleware } from '../shared/middlewares';
 
 @Module({
   imports: [AuthModule, MemberModule, BookModule, BorrowModule],
   controllers: [],
   providers: [],
 })
-export class ApiModule {}
+export class ApiModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthenticationMiddleware)
+      .exclude(
+        { path: 'auth/sign-up', method: RequestMethod.POST },
+        { path: 'auth/sign-in', method: RequestMethod.POST },
+      )
+      .forRoutes('*');
+  }
+}
